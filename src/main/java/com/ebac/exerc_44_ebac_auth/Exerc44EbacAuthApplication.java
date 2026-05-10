df8edcf62a4efd72c2373bee746e3bed86a5de3f
beeleza <higorbeleza.dev@gmail.com>
2026-05-09 23:23:34 -0300

    first commit

---
+++ src/main/java/com/ebac/exerc_44_ebac_auth/Exerc44EbacAuthApplication.java
@@ -0,0 +1,13 @@
+package com.ebac.exerc_44_ebac_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exerc44EbacAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exerc44EbacAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebac/exerc_44_ebac_auth/Exerc44EbacAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebac.exerc_44_ebac_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exerc44EbacAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
